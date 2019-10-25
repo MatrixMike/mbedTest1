b0c9db1f1709ea070bf9ccee5b93861e667fe36d
Mike J H <matrixmike@icloud.com>
2019-10-25 13:23:14 +1100

    testing git and mbed relationship

--- main.cpp
+++ main.cpp
@@ -27,16 +27,14 @@ DEALINGS IN THE SOFTWARE.
 // testing the clone system 
 // using hg 
 // 18.10.2019 13:46:18
+// 25.10.2019 13:22:26
 MicroBit uBit;
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
-
-    // Insert your code here!
     uBit.display.scroll("HELLO WORLD! :)");
-
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
     // sit in the idle task forever, in a power efficient sleep.
